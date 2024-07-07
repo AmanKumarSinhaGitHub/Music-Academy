e7f58689ac6a7986e6e3dff9e9806c9a7b457b33
Aman Kumar Sinha <amankrsinha07@gmail.com>
2024-07-07 19:39:11 +0530

    added navbar using aceternity ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-center text-2xl mt-20">Music Academy</h1>
+      <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+        <h1 className="text-center text-2xl mt-20">Music Academy</h1>
+      </main>
     </>
   );
 }
