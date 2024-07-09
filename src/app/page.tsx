ac6713f90e3ab502cdf7d305397ce4d6fe0cf3a4
Aman Kumar Sinha <amankrsinha07@gmail.com>
2024-07-09 23:58:37 +0530

    added courses section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <>
       <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
         <HeroSection/>
+        <FeaturedCourses/>
       </main>
     </>
   );
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -16,7 +16,7 @@ const HeroSection = () => {
                 <div className="p-4 relative z-10 w-full text-center" >
                     <h1
                         className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400"
-                    >Master the art of music</h1>
+                    >Master the Art of Music</h1>
                     <p
                         className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto"
                     >Dive into our comprehensive music courses and transform your musical journey today. Whether you&apos;re a beginner or looking to refine your skills, join us to unlock your true potential.</p>
