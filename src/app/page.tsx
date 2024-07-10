599787ca96e86c33347cc256cd219224db6cccb8
Aman Kumar Sinha <amankrsinha07@gmail.com>
2024-07-10 22:45:26 +0530

    added testimonals section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import Testimonials from "@/components/Testimonials";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
         <HeroSection />
         <FeaturedCourses />
         <WhyChooseUs />
+        <Testimonials/>
       </main>
     </>
   );
