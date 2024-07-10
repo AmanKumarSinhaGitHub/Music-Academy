38555c7a237b3d642d7409b27db6427e74e79a83
Aman Kumar Sinha <amankrsinha07@gmail.com>
2024-07-10 21:47:32 +0530

    added why choose us page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <>
       <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-        <HeroSection/>
-        <FeaturedCourses/>
+        <HeroSection />
+        <FeaturedCourses />
+        <WhyChooseUs />
       </main>
     </>
   );
