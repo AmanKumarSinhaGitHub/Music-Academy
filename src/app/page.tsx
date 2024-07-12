d559d51fe59b07342193b6e5263b815214358288
Aman Kumar Sinha <amankrsinha07@gmail.com>
2024-07-12 21:36:10 +0530

    added footer section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import Instructor from "@/components/Instructor";
 import Testimonials from "@/components/Testimonials";
@@ -15,6 +16,7 @@ export default function Home() {
         <Testimonials/>
         <UpcomingWebminars/>
         <Instructor/>
+        <Footer/>
       </main>
     </>
   );
