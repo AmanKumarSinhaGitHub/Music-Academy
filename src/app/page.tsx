56e13fb2eaca24473062fc9b22bc6918010afc95
Aman Kumar Sinha <amankrsinha07@gmail.com>
2024-07-12 08:27:15 +0530

    added instructors section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import Instructor from "@/components/Instructor";
 import Testimonials from "@/components/Testimonials";
 import UpcomingWebminars from "@/components/UpcomingWebminars";
 import WhyChooseUs from "@/components/WhyChooseUs";
@@ -13,6 +14,7 @@ export default function Home() {
         <WhyChooseUs />
         <Testimonials/>
         <UpcomingWebminars/>
+        <Instructor/>
       </main>
     </>
   );
